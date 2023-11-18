775160e87a80b77d0316870000ac6b04d8c24254
godwill <godwillkisia5@gmail.com>
2023-11-18 17:20:00 +0300

    update

--- lib/screens/confirmation.dart
+++ lib/screens/confirmation.dart
@@ -1,21 +1,22 @@
-
-
 import 'package:flutter/material.dart';
 
 class ConfirmationPage extends StatefulWidget {
-
-
-@override
-Confirmation createState() => Confirmation();
-
+  @override
+  Confirmation createState() => Confirmation();
 }
 
 class Confirmation extends State<ConfirmationPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(appBar: AppBar(title: const Text('Successful'),),body: Container(),),)
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Successful'),
+        ),
+        body: Container(
+          child: SizedBox(child: FittedBox()),
+        ),
+      ),
+    );
   }
-
-
-
 }
